5c2b97fefb6d8407677db0c35fc3c011d77f587a
Kazuiin <deeboballan@outlook.com>
2026-01-09 13:59:47 +0000

    fixed transform snapping with help from VIOLET!!!!!!!!

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@
 const dragWM = document.querySelectorAll(".dragWM");
 
     dragWM.forEach((dragWM) => {
-        
-         const mousePos = (e) => {
-            dragWM.style.top = e.pageY + "px";
-            dragWM.style.left = e.pageX + "px";
+        let startX = 0;
+        let startY = 0;
+        const mousePos = (e) => {
+            startY = startY + e.movementY
+            startX = startX + e.movementX
+            dragWM.parentElement.style.top = startY + "px";
+            dragWM.parentElement.style.left = startX + "px";
+
             };
 
             dragWM.addEventListener("mousedown", () => {
+                startX = dragWM.parentElement.getBoundingClientRect().left;
+                startY = dragWM.parentElement.getBoundingClientRect().top;
+                if (isNaN(startX)) startX = 0;
+                if (isNaN(startY)) startY = 0;
                 window.addEventListener("mousemove", mousePos);
             });
 
