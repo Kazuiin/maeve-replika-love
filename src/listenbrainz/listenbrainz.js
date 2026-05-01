26717b57ebf1e683409d235c70b19cd0acf4cf38
Kazuiin <deeboballan@outlook.com>
2026-05-01 23:39:58 +0100

    hi thing hi thing i did thing

--- src/listenbrainz/listenbrainz.js
+++ src/listenbrainz/listenbrainz.js
@@ -6,6 +6,7 @@ const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
     // elements
 const nowPlayingHTML = document.getElementById("nowlistening")
 const nowPlayingCover = document.getElementById("nowListeningCover")
+const coverLink = document.getElementById("listenbrainzlink")
 
 const listen1HTML = document.getElementById("listen1")
 const listen2HTML = document.getElementById("listen2")
@@ -16,20 +17,26 @@ fetch_listens()
     // functions 
 function fetch_listens() {
     fetch_json_retry(now_playing_url, 2).then((nowPlayingData) => {
-        nowPlayingHTML.innerHTML = "now playing!" + '<br>' + nowPlayingData.payload.listens[0].track_metadata.artist_name + " - " +nowPlayingData.payload.listens[0].track_metadata.track_name;
+        nowPlayingHTML.innerHTML = "now playing!" + '<br><br>' + nowPlayingData.payload.listens[0].track_metadata.artist_name 
+        + '<br>' +nowPlayingData.payload.listens[0].track_metadata.track_name  + '<br>' + nowPlayingData.payload.listens[0].track_metadata.release_name;
         nowPlayingCover.setAttribute('src', 'https://coverartarchive.org/release/' + nowPlayingData.payload.listens[0].track_metadata.additional_info.release_mbid + '/front-250.jpg')
+        nowPlayingCover.setAttribute('title', nowPlayingData.payload.listens[0].track_metadata.artist_name + " - " + nowPlayingData.payload.listens[0].track_metadata.release_name + " on listenbrainz!")
+        coverLink.setAttribute('href', 'https://listenbrainz.org/album/' + nowPlayingData.payload.listens[0].track_metadata.additional_info.release_group_mbid)
         console.log(nowPlayingData)
     });
     fetch_json_retry(listens_url, 2).then((listensData) => {
-        listen1HTML.innerHTML = "recent listens!" + '<br>' + listensData.payload.listens[0].track_metadata.artist_name + " - " + listensData.payload.listens[0].track_metadata.track_name;
+        listen1HTML.innerHTML = "recent listens!" + '<br><br>' + listensData.payload.listens[0].track_metadata.artist_name + '<br>'
+         + listensData.payload.listens[0].track_metadata.track_name + '<br>' + listensData.payload.listens[0].track_metadata.release_name;
         console.log(listensData)
     });
     fetch_json_retry(listens_url, 2).then((listensData) => {
-        listen2HTML.innerHTML = '<br>' + listensData.payload.listens[1].track_metadata.artist_name + " - " + listensData.payload.listens[1].track_metadata.track_name;
+        listen2HTML.innerHTML = '<br><br>' + listensData.payload.listens[1].track_metadata.artist_name + '<br>'
+         + listensData.payload.listens[1].track_metadata.track_name + '<br>' + listensData.payload.listens[1].track_metadata.release_name;
         console.log(listensData)
     });
     fetch_json_retry(listens_url, 2).then((listensData) => {
-        listen3HTML.innerHTML = '<br>' + listensData.payload.listens[2].track_metadata.artist_name + " - " + listensData.payload.listens[2].track_metadata.track_name;
+        listen3HTML.innerHTML = '<br><br>' + listensData.payload.listens[2].track_metadata.artist_name + '<br>'
+         + listensData.payload.listens[2].track_metadata.track_name + '<br>' + listensData.payload.listens[2].track_metadata.release_name;
         console.log(listensData)
     });
 
