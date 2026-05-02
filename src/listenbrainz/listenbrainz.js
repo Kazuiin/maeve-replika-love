4a446d99f3be099d579dbd77e0ac453a9f7fcf87
Kazuiin <deeboballan@outlook.com>
2026-05-02 08:46:58 +0100

    reorganised

--- src/listenbrainz/listenbrainz.js
+++ src/listenbrainz/listenbrainz.js
@@ -7,7 +7,6 @@ const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 const nowPlayingHTML = document.getElementById("nowlistening")
 const nowPlayingCover = document.getElementById("nowListeningCover")
 const coverLink = document.getElementById("listenbrainzlink")
-
 const listen1HTML = document.getElementById("listen1")
 const listen2HTML = document.getElementById("listen2")
 const listen3HTML = document.getElementById("listen3")
@@ -17,26 +16,33 @@ fetch_listens()
     // functions 
 function fetch_listens() {
     fetch_json_retry(now_playing_url, 2).then((nowPlayingData) => {
-        nowPlayingHTML.innerHTML = "now playing!" + '<br><br>' + nowPlayingData.payload.listens[0].track_metadata.artist_name 
-        + '<br>' +nowPlayingData.payload.listens[0].track_metadata.track_name  + '<br>' + nowPlayingData.payload.listens[0].track_metadata.release_name;
-        nowPlayingCover.setAttribute('src', 'https://coverartarchive.org/release/' + nowPlayingData.payload.listens[0].track_metadata.additional_info.release_mbid + '/front-250.jpg')
-        nowPlayingCover.setAttribute('title', nowPlayingData.payload.listens[0].track_metadata.artist_name + " - " + nowPlayingData.payload.listens[0].track_metadata.release_name + " on")
-        coverLink.setAttribute('href', 'https://listenbrainz.org/album/' + nowPlayingData.payload.listens[0].track_metadata.additional_info.release_group_mbid)
-        console.log(nowPlayingData)
+        const nowPlayingDataPath = nowPlayingData.payload.listens[0].track_metadata
+        console.log("nowPlayingArray Length: " + nowPlayingData.arrayLength)
+        if (nowPlayingData) {
+            console.log(nowPlayingData)
+            nowPlayingHTML.innerHTML = "now playing!" + '<br><br>' + nowPlayingDataPath.artist_name 
+            + '<br>' + nowPlayingDataPath.track_name  + '<br>' + nowPlayingDataPath.release_name;
+            nowPlayingCover.setAttribute('src', 'https://coverartarchive.org/release/' + nowPlayingDataPath.additional_info.release_mbid + '/front-250.jpg')
+            nowPlayingCover.setAttribute('title', nowPlayingDataPath.artist_name + " - " + nowPlayingDataPath.track_metadata.release_name + " on listenbrainz!!!")
+            coverLink.setAttribute('href', 'https://listenbrainz.org/album/' + nowPlayingDataPath.additional_info.release_group_mbid)
+        }
+         else {
+            console.log("listenbrainz is sleepy...")
+            nowPlayingHTML.innerHTML = "now playing!" + '<br><br>' + "listenbrainz is sleepy,,,";
+            nowPlayingCover.setAttribute('src', 'src/assets/images/album covers/fallback.png')
+            nowPlayingCover.setAttribute('title', '')
+        }
     });
     fetch_json_retry(listens_url, 2).then((listensData) => {
         console.log(listensData)
-            // 1
-        listen1HTML.innerHTML = "recent listens!" + '<br><br>' + listensData.payload.listens[0].track_metadata.artist_name + '<br>'
-         + listensData.payload.listens[0].track_metadata.track_name + '<br>' + listensData.payload.listens[0].track_metadata.release_name;
-            // 2
-        listen2HTML.innerHTML = '<br><br>' + listensData.payload.listens[1].track_metadata.artist_name + '<br>'
-         + listensData.payload.listens[1].track_metadata.track_name + '<br>' + listensData.payload.listens[1].track_metadata.release_name;
-            // 3
-        listen3HTML.innerHTML = '<br><br>' + listensData.payload.listens[2].track_metadata.artist_name + '<br>'
-         + listensData.payload.listens[2].track_metadata.track_name + '<br>' + listensData.payload.listens[2].track_metadata.release_name;
+            function feedListen(listenElement, listenValue) {
+                const dataPath = listensData.payload.listens[listenValue].track_metadata
+                listenElement.innerHTML = "recent listens!" + '<br><br>' + dataPath.artist_name + '<br>' + dataPath.track_name + '<br>' + dataPath.release_name;
+            }
+            feedListen(listen1HTML, 0)
+            feedListen(listen2HTML, 1)
+            feedListen(listen3HTML, 2)
     });
-
 }
  setInterval(() => {
     fetch_listens()
