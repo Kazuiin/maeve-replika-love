d090ce05b262b0bc6206f08bf1ca48f4ebdcf7be
Kazuiin <deeboballan@outlook.com>
2026-05-12 04:38:20 +0100

    updated a lil :3

--- noteGenerator.js
+++ noteGenerator.js
@@ -6,7 +6,7 @@ let pagesDirectory = fs.readdirSync("notes/togenerate/");
 for (let fileName of pagesDirectory) {
     let noteFile = fs.readFileSync("notes/togenerate/" + fileName);
     const relativeNotesPath = "notes/pages/"
-
+    
         // generates new folders with an index.html inside
     if (!fs.existsSync(relativeNotesPath)) {
         fs.mkdirSync(relativeNotesPath);
