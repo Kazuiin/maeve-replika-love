0af6e1851db1a8b3d7803485ca95aa1f12331c7c
Kazuiin <deeboballan@outlook.com>
2026-05-12 16:12:43 +0100

    converted note generator to use markdown and a template :3

--- noteGenerator.js
+++ noteGenerator.js
@@ -1,20 +1,29 @@
     // variables
+const showdown = require("showdown")
 const fs = require("fs");
+const { parse } = require("path");
+const converter = new showdown.Converter()
 let pagesDirectory = fs.readdirSync("notes/togenerate/");
+const templateFile = fs.readFileSync("notes/template.html").toString()
 
     // checks directory reads all files inside
 for (let fileName of pagesDirectory) {
-    let noteFile = fs.readFileSync("notes/togenerate/" + fileName);
+    let noteFileMD = fs.readFileSync("notes/togenerate/" + fileName);
     const relativeNotesPath = "notes/pages/"
     
         // generates new folders with an index.html inside
     if (!fs.existsSync(relativeNotesPath)) {
         fs.mkdirSync(relativeNotesPath);
     }
-    let noteDir = relativeNotesPath + fileName.replace(".html", "")
+    let noteDir = relativeNotesPath + fileName.replace(".md", "").replace(".html", "")
     if (!fs.existsSync(noteDir)) {
         fs.mkdirSync(noteDir);
-        console.log(fs.readdirSync(relativeNotesPath))
     }
-    fs.writeFileSync(noteDir + "/index.html", noteFile);
+    let noteFileHTML = converter.makeHtml(noteFileMD.toString())
+    const fileNameDate = fileName.replace(".md", "").split("-")
+    let parsedDate = new Date(parseInt(fileNameDate[2], 10),parseInt(fileNameDate[1], 10) - 1, parseInt(fileNameDate[0])).toLocaleDateString("en-GB",
+    {weekday: "long", year: "numeric", month: "long", day: "numeric"});
+        // writes to template file
+    fs.writeFileSync(noteDir + "/index.html", templateFile.replace("{content}", noteFileHTML)
+    .replace('{date}', parsedDate));
 }
