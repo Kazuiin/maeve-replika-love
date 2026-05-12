397cbecb79c090381714e7fe44447b58bba99a35
Kazuiin <deeboballan@outlook.com>
2026-05-12 02:19:33 +0100

    generator tweaks :3

--- noteGenerator.js
+++ noteGenerator.js
@@ -1,15 +1,20 @@
     // variables
 const fs = require("fs");
-let pagesDirectory = fs.readdirSync("notes/pages/");
+let pagesDirectory = fs.readdirSync("notes/togenerate/");
 
-console.log(pagesDirectory)
-
+    // checks directory reads all files inside
 for (let fileName of pagesDirectory) {
-    let noteFile = fs.readFileSync("notes/pages/" + fileName);
-    const relativeNotesPath = "notes/generated/"
+    let noteFile = fs.readFileSync("notes/togenerate/" + fileName);
+    const relativeNotesPath = "notes/pages/"
+    if (!fs.existsSync(relativeNotesPath)) {
+        fs.mkdirSync(relativeNotesPath);
+    }
+
+        // generates new folders with an index.html inside
     let noteDir = relativeNotesPath + fileName.replace(".html", " ")
     if (!fs.existsSync(noteDir)) {
         fs.mkdirSync(noteDir);
+        console.log(fs.readdirSync(relativeNotesPath) + "meow")
     }
     fs.writeFileSync(noteDir + "/index.html", noteFile);
 }
