499e1ebad2935c41d8e52451987d0977dead340d
Kazuiin <deeboballan@outlook.com>
2026-05-17 03:21:17 +0100

    moved stuff around and commented

--- src/operatingsystem/terminal.js
+++ src/operatingsystem/terminal.js
@@ -10,10 +10,6 @@ const deviceFetches = {
     "kolibri": `${newDevice("kolibri", "RaspbianOS", "Raspberry Pi 5", "gallant 12x22 (12pt)", "Broadcom BCM2712 @ 2.40GHz", "VideoCore V3D VII @ 1.20GHz", "8GB", ["/"], ["512GB"], "raspbianos.png")}`,
     "ara": `${newDevice("ara", "NixOS", "HP Fortis Yavilla", "gallant 12x22 (12pt)", "Intel Core N100 @ 3.40GHz", "Intel UHD Graphics @ 0.75GHz", "8GB", ["/"], ["64GB"], "nixos.png")}`      
 }
-
-console.log(deviceFetches["elster"])
-
-
 const commands = {
     
     "help": help,
@@ -23,56 +19,7 @@ const commands = {
     "ssh": ssh,
     "clear": clear
 }
-
-input.addEventListener("keydown", function(event) {
-    if(event.code == "Enter") {
-        commandInput = input.value;
-        parsedCommand = commandInput.split(" ")
-        console.log(parsedCommand)
-        if (!(parsedCommand[0] in commands)) {
-            output.innerHTML += `<div class="errorMsg cli">${commandInput}: command not found</div>`;
-        }
-        
-        else if (parsedCommand[0] == "clear") {
-            output.innerHTML = ""
-        }
-
-        else {
-            output.innerHTML += commands[parsedCommand[0]]();
-        }
-        input.value = "";
-    }
-})
-
-function neofetch() {
-    let result = deviceFetches[device];
-    return result
-}
-
-function ssh() {
-    let result = "";
-    if (parsedCommand[1] in deviceFetches) {
-        device = parsedCommand[1];
-        console.log(device);
-        result = `<div class="deviceID cli">connected to ${device}.replika.love!</div>`;
-    }
-    else {
-        result = `<div class="errorMsg cli">could not resolve hostname.</div<`;
-    }
-    return result
-}
-
-function clear() {
-    console.log("cleared")
-}
-
-
-function help() {
-    let helpList = `<div class="helpList cli"><nobr> clear neofetch ssh<br><br><b class="yellow">use --help to get more help on a specific command</b></nobr></div>`
-
-    return helpList
-}
-
+    // makes devices
 function newDevice(device, os, host, font, cpu, gpu, ram, diskNames, diskCapacities, ascii) {
     let forCounter = 0;
     let Fetch =
@@ -96,4 +43,50 @@ function newDevice(device, os, host, font, cpu, gpu, ram, diskNames, diskCapacit
             }
         }
     return Fetch;
+}
+    // checks textbox
+input.addEventListener("keydown", function(event) {
+    if(event.code == "Enter") {
+        commandInput = input.value;
+        parsedCommand = commandInput.split(" ")
+        console.log(parsedCommand)
+        if (!(parsedCommand[0] in commands)) {
+            output.innerHTML += `<div class="errorMsg cli">${commandInput}: command not found</div>`;
+        }
+        else if (parsedCommand[0] == "clear") {
+            output.innerHTML = ""
+        }
+        else {
+            output.innerHTML += commands[parsedCommand[0]]();
+        }
+        input.value = "";
+    }
+})
+    // commands
+function neofetch() {
+    let result = deviceFetches[device];
+    return result
+}
+
+function ssh() {
+    let result = "";
+    if (parsedCommand[1] in deviceFetches) {
+        device = parsedCommand[1];
+        console.log(device);
+        result = `<div class="deviceID cli">connected to ${device}.replika.love!</div>`;
+    }
+    else {
+        result = `<div class="errorMsg cli">could not resolve hostname.</div<`;
+    }
+    return result
+}
+
+function clear() {
+    console.log("cleared")
+}
+
+function help() {
+    let helpList = `<div class="helpList cli"><nobr> clear neofetch ssh<br><br><b class="yellow">use --help to get more help on a specific command</b></nobr></div>`
+
+    return helpList
 }
