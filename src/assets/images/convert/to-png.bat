bacf2d27844a73220b7cc0ecf373b641f6d0e928
Kazuiin <deeboballan@outlook.com>
2026-06-09 17:29:48 +0100

    optimised batch scripts :3

--- src/assets/images/convert/to-png.bat
+++ src/assets/images/convert/to-png.bat
@@ -1,21 +1,3 @@
 echo converting all images to png
 
-for %%i in ("*.jpg") do "ffmpeg.exe" -n -i "%%i" -f image2 -c:v copy -q:v 1 "%%~ni.png"
-
-for %%i in ("*.jpeg") do "ffmpeg.exe" -n -i "%%i" -f image2 -c:v copy -q:v 1 "%%~ni.png"
-
-for %%i in ("*.png") do "ffmpeg.exe" -n -i "%%i" -f image2 -c:v copy -q:v 1 "%%~ni.png"
-
-for %%i in ("*.png") do "ffmpeg.exe" -n -i "%%i" -f image2 -c:v copy -q:v 1 "%%~ni.png"
-
-for %%i in ("*.gif") do "ffmpeg.exe" -n -i "%%i" -f image2 -c:v copy -q:v 1 "%%~ni.png"
-
-for %%i in ("*.tiff") do "ffmpeg.exe" -n -i "%%i" -f image2 -c:v copy -q:v 1 "%%~ni.png"
-
-for %%i in ("*.avif") do "ffmpeg.exe" -n -i "%%i" -f image2 -c:v copy -q:v 1 "%%~ni.png"
-
-for %%i in ("*.exr) do "ffmpeg.exe" -n -i "%%i" -f image2 -c:v copy -q:v 1 "%%~ni.png"
-
-for %%i in ("*.webp) do "ffmpeg.exe" -n -i "%%i" -f image2 -c:v copy -q:v 1 "%%~ni.png"
-
-for %%i in ("*.dng) do "ffmpeg.exe" -n -i "%%i" -f image2 -c:v copy -q:v 1 "%%~ni.png"
+for %%i in ("*.jpg", "*.jpeg", "*.png", "*.gif", "*.tiff", "*.avif", "*.exr", "*.webp", "*.dng") do "ffmpeg.exe" -n -i "%%i" -f image2 -c:v copy -q:v 1 "output/%%~ni.png"
